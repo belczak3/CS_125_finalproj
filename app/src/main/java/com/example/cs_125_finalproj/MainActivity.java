f00617bda87fa46ce4d508affc4aba8bf48ac999
sharizag <aghassi2@illinois.edu>
2020-04-27 23:29:25 -0500

    I added a comment describing the general structure of the UI interface.

--- app/src/main/java/com/example/cs_125_finalproj/MainActivity.java
+++ app/src/main/java/com/example/cs_125_finalproj/MainActivity.java
@@ -11,4 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /** The main activity will contain the labels of terms needed for a first or second linear
+     * differential equation. There will be some sort of input to type the coefficients associated
+     * with each term. There will also be a place to insert the symbol for the independent variable
+     * so that if any other letters are types into the inputs, the application recognizes them as
+     * constants. We will need to use some sort of Java math library that deals with calculus so
+     * that the math for the coding is easier. There will be a calculate button, that will print
+     * the solution to the differential equation.*/
 }
